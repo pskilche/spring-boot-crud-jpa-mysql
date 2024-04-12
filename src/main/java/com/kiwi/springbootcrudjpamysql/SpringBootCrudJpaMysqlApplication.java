cab0f895191c8fe76a8f5d14fcc37ac2bb01b394
pkilche <pkilche@ebates.com>
2024-04-12 14:48:03 +0530

    Pushing the initial project structure

---
+++ src/main/java/com/kiwi/springbootcrudjpamysql/SpringBootCrudJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.kiwi.springbootcrudjpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudJpaMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiwi/springbootcrudjpamysql/SpringBootCrudJpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiwi.springbootcrudjpamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudJpaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
